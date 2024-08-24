66c7cc62e334603241d8ef285c68b571209702cf
Nazar0929 <166243366+Nazar0929@users.noreply.github.com>
2024-08-24 18:28:28 +0300

    leo

--- script.js
+++ script.js
@@ -16,16 +16,67 @@ for (let i = 0; i < elements.length; i++) {
 }
 
 
+
 let horizontScroll = document.querySelector('.scrol');
-let leftBtn = document.getElementById('leftBtn')
-let rightBtn = document.getElementById('rightBtn')
+let leftBtn = document.getElementById('leftButton');
+let leftBtnn = document.getElementById('leftBtnn');
+let rightBtn = document.getElementById('rightBtn');
+let rightBut = document.getElementById('rightBut');
+let leftBut = document.getElementById('leftBut');
 
 rightBtn.addEventListener('click', ()=> {
     horizontScroll.style.scrollBehavior = "smooth";
     horizontScroll.scrollLeft += 1274;
 });
 
-leftBtn.addEventListener('click', ()=> {
+leftBtnn.addEventListener('click', ()=> {
     horizontScroll.style.scrollBehavior = "smooth";
-    horizontScroll.scrollRight += 1274;
+    horizontScroll.scrollLeft -= 1274;
+});
+
+leftButton.addEventListener('click', ()=> {
+    horizontScroll.style.scrollBehavior = "smooth";
+    horizontScroll.scrollLeft -= 1274;
+});
+
+leftBut.addEventListener('click', ()=> {
+    horizontScroll.style.scrollBehavior = "smooth";
+    horizontScroll.scrollLeft -= 1274;
+});
+
+rightBut.addEventListener('click', ()=> {
+    horizontScroll.style.scrollBehavior = "smooth";
+    horizontScroll.scrollLeft += 1274;
+});
+
+
+
+function onEntry(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+        change.target.classList.add('element-show');
+      } else {
+        change.target.classList.remove('element-show');
+      }
+    });
+  }
+  let options = { threshold: [0.4] };
+  let observer = new IntersectionObserver(onEntry, options);
+  let elementss = document.querySelectorAll('.container');
+  for (let elm of elementss) {
+    observer.observe(elm);
+  }
+
+  document.getElementById('toggle-section-btn').addEventListener('click', function() {
+    // Получаем скрытую секцию
+    const section = document.getElementById('hidden-section');
+    
+    // Проверяем текущее состояние секции и переключаем видимость
+    if (section.classList.contains('hidden')) {
+        section.classList.remove('hidden');
+        this.textContent = 'Hide Program'; // Меняем текст кнопки
+    } else {
+        section.classList.add('hidden');
+        this.textContent = 'Load More'; // Меняем текст кнопки обратно
+    }
 });
